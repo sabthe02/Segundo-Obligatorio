a39e4b30df9499507476e9999af21074fcf98843
RamiroCaceres07 <ramirocaba07@gmail.com>
2023-11-18 01:47:47 -0300

    cambios ramiro - consultas

--- Consulta.cpp
+++ Consulta.cpp
@@ -1,37 +1,34 @@
 #include "Consulta.h"
 
-void desplegarConsulta (Consulta c) {
-desplegarFecha(c.fec);
-printf("\nCedula de la consulta: %ld", c.cedulaEnConsulta);
-print(c.motivo);
-desplegarEvaluacion (c.evaluacion);
+void desplegarConsulta(Consulta c) {
+    desplegarFecha(c.fec);
+    printf("\nCedula de la consulta: %ld", c.cedulaEnConsulta);
+    print(c.motivo);
+    desplegarEvaluacion(c.evaluacion);
 }
 
-void cargarConsulta (Consulta &c) {
-cargarFecha(c.fec);
-printf("\nIngrese cedula de la consulta: ");
-scanf("%ld", &c.cedulaEnConsulta);
-scan (c.motivo);
-cargarEvaluacion(c.evaluacion);
-
+void cargarConsulta(Consulta &c) {
+    cargarFecha(c.fec);
+    printf("\nIngrese cedula de la consulta: ");
+    scanf("%ld", &c.cedulaEnConsulta);
+    printf("\nIngrese el motivo de la consulta: ");
+    scan(c.motivo);
+    printf("\nIngrese la evaluacion de la consulta: ");
+    cargarEvaluacion(c.evaluacion);
 }
 
-fecha seleccionarFecha (Consulta c) {
-return c.fec;
-
+fecha seleccionarFecha(Consulta c) {
+    return c.fec;
 }
 
-long int seleccionarCedulaConsulta (Consulta c) {
-return c.cedulaEnConsulta;
-
+long int seleccionarCedulaConsulta(Consulta c) {
+    return c.cedulaEnConsulta;
 }
 
-String seleccionarMotivo (Consulta c) {
-return c.motivo;
-
+String seleccionarMotivo(Consulta c) {
+    return c.motivo;
 }
 
-Evaluacion seleccionarEvaluacion (Consulta c) {
-return c.evaluacion;
-
+Evaluacion seleccionarEvaluacion(Consulta c) {
+    return c.evaluacion;
 }
